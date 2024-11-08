f23687c0b45ec9ebf273104f5d5b2d1eeb828e3b
Bryan-Menoux <bryan.menoux@edu.univ-fcomte.fr>
2024-11-08 14:09:00 +0100

    exercice 3

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,15 @@
 
 let aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
 aMettreEnRouge.style.color = "red";
+
+let enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+enRougeSuiteAClick.addEventListener("click", (evt) => {
+  enRougeSuiteAClick.style.color = "red";
+});
+
+var collH2 = document.querySelectorAll("H2");
+collH2.forEach((elm) => {
+  elm.addEventListener("click", (evt) => {
+    evt.target.style.color = "red";
+  });
+});
